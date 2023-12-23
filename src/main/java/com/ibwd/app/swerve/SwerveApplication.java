6e45ec125a4c34ae33a6964d90c70e8abe2fe4c9
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2023-12-23 05:31:51 -0600

    first commit

---
+++ src/main/java/com/ibwd/app/swerve/SwerveApplication.java
@@ -0,0 +1,13 @@
+package com.ibwd.app.swerve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwerveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwerveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibwd/app/swerve/SwerveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibwd.app.swerve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwerveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
